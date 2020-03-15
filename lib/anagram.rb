25d6f993665145abcedf75971adee97b771a4e99
Pharia Le <pharia.le@gmail.com>
2020-03-15 17:00:01 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ class Anagram
     # word = "listen"
     # list = ['apple', 'cat', 'inlets']
     list.detect do |list_word|
-      (word - word_list).empty? || (win - o_spaces).empty?
+      (word - word_list).empty?
     end
       
   end
